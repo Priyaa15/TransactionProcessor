01b9561f3924a2edee2f6cd877ca826dbd3519e8
Ppriya <priyasilverback@gmail.com>
2025-11-24 22:17:40 +1100

    Clean transactions Method Added

--- src/etl/transaction_processor.py
+++ src/etl/transaction_processor.py
@@ -1,5 +1,6 @@
 import csv
 import logging
+from datetime import datetime
 
 # Set up logger
 logging.basicConfig(
@@ -30,6 +31,39 @@ class transaction_processor:
     except Exception as e:
         logger.info(f"Error Reading transactions: {e}")
         raise
+
+    #2. Clean and validate the data 
+
+    def clean_data(self):
+        """Clean and validate transaction data"""
+        cleaned = []
+
+        for tx in self.transactions:
+            try:
+                # convert amount into float
+                tx['amount'] = float(tx['amount'])
+
+                # validate timestamp format
+                datetime.strptime(tx['timestamp'], '%y-%m-%d %H:%M:%S')
+
+                #Validate transaction type
+                if tx['transaction_type'] not in ['debit', 'credit']:
+                    raise ValueError(f"Invalid transaction type: {tx['transaction_type']}")
+                
+                #Validate status
+                if tx['status'] not in ['posted', 'failed', 'refunded']:
+                    raise ValueError(f"Invalid status {tx['status']}")
+                
+                cleaned.append(tx)
+            except ValueError as e:
+                logger.error(f"invalid transaction: {tx['transaction_id']}{e}")
+                self.error_records.append({
+                    'transaction_id': tx['transaction_id'],
+                    'error': str(e)
+                })
+            
+            self.transactions = cleaned
+            logger.info(f"Successfully cleaned transactions: {len(cleaned)} transactions")
 
 
         
